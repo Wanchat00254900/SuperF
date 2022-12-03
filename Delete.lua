90742951e205317f824f885e424e3cedd043577b
Easy Hub <107304900+Wanchat00254900@users.noreply.github.com>
2022-12-03 17:53:10 +0700

    Del

--- Delete.lua
+++ Delete.lua
@@ -1,6 +1,10 @@
 game.Lighting.FogEnd = 200
 for i,v in pairs(workspace:GetDescendants()) do
-if v.Name == "Home" or v.Name == "Flower" or v.Name == "Sea" or v.Name = "Ocean" or v.Name == "Seas" then
-v:Destroy()
-end
+if v:IsA("UnionOperation") or v:IsA("MeshPart") then
+   if v.Parent:FindFirstChild("Humanoid") then
+      
+      else
+       v:Destroy()
+      end
+   end
 end
