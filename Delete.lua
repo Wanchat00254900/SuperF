b233fa15ab481e83ff7ae2e2942df0ada094e889
Easy Hub <107304900+Wanchat00254900@users.noreply.github.com>
2022-12-03 17:49:20 +0700

    Del.lua

--- Delete.lua
+++ Delete.lua
@@ -1,8 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Wanchat00254900/lowmode/main/fastmode.lua",true))()
-game.Lighting.FogColor = Color3.new(0,0,0)
-game.Lighting.FogEnd = 1000
+game.Lighting.FogEnd = 200
 for i,v in pairs(workspace:GetDescendants()) do
-if v:IsA("WedgePart") or v.Name == "Home" or v.Name == "Flower" or v.Name == "Sea" or v.Name = "Ocean" or v.Name == "Seas" then
+if v.Name == "Home" or v.Name == "Flower" or v.Name == "Sea" or v.Name = "Ocean" or v.Name == "Seas" then
 v:Destroy()
 end
 end
